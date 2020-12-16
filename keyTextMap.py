95d994a3b8050ce60d0e4b1c1967a40019d05a29
rksmile <40001585+rksmile@users.noreply.github.com>
2020-12-16 20:22:30 +0100

    Update keyTextMap.py

--- keyTextMap.py
+++ keyTextMap.py
@@ -1,18 +1,6 @@
 import random 
 from PIL import Image
 
-'''
-One idea for random number generation for the key is
-to create a Pseudo RNG algorithm such that all generated
-numbers are deterministic and reproducable 
-given a starting nu,ber/sequence and equip the encrypter
-and decrypter with the algorithm. Thus the channel only 
-has to send the first numer/sequence and the decrypter 
-can use that number/sequence to regenerate the key 
-for decryption. It has to be secure and with a hard to 
-crack pattern for use in real cryptographic problems
-but in our case a simple one should do fine	
-'''
 #for now we use random.randint
 class KeyMap():
 	def __init__(self, imageSize):
@@ -33,9 +21,5 @@ class KeyMap():
 				else:
 					temp.append(0)
 			self.keyMatrix.append(temp)
-
-
-
-
 
 
